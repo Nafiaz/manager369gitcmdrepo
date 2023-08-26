1d86c8d16e13380db02a7a6825489a5a9f0ea19b
Nafiaz <riyaz.skr369@gmail.com>
2023-08-26 18:01:55 +0530

    my first commit

---
+++ src/test/java/tests/TestOne.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestOne 
+{
+   @Test
+   public void method()
+   {
+	   Reporter.log("Hello Batchmates");
+   }
+}
